000e6e05f373e45063d666c8e712391179f6291d
Curtisww17 <wcvikingdude@gmail.com>
2018-12-02 18:11:12 -0500

    Play multiple hands with this update

--- src/Poker.java
+++ src/Poker.java
@@ -15,11 +15,36 @@ public class Poker{
 		System.out.println("And how many computer opponents would you like?");
 		int numComp = getNum();
 		Game g = new Game(numHuman,numComp);
-		while (true) {
+		boolean playAgain = true;
+		do {
 			g.playHand();
 			g.reset();
-		}
+			playAgain = PlayAgain();
+		} while (playAgain);
 	}
+	
+	private static boolean PlayAgain() {
+		System.out.println("\nDo you want to play another hand? (y,n)");
+		char answer = ' ';
+		boolean isGood = false;
+		while (!isGood) {
+			Scanner scan = new Scanner(System.in);
+			if (scan.hasNext())
+				answer = scan.next().toLowerCase().charAt(0);
+			else {
+				System.out.println("That is not an answer.");
+			}
+			if (answer == 'y' || answer == 'n') {
+				isGood = true;
+			} else {
+				System.out.println("Answer must be either \"y\" or \"n\".");
+			}
+		}
+		if (answer == 'y') {
+			return true;
+		} else return false;
+	}
+	
 	
 	private static int getNum() {
 		int num = 0;
