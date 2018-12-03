775a6467cf6d49171769ee3cb6e1b779e67a0a42
Adam <30380231+acramernc@users.noreply.github.com>
2018-12-02 19:00:39 -0500

    Finished score method (Still untested)

--- src/Score.java
+++ src/Score.java
@@ -6,12 +6,9 @@ import java.util.TreeMap;
 
 /*
  * Known Issues:
- * anything lower than 4 of a kind is not checked
- * royal flush doesnt follow the rules correctly
  * if there is a tie the method only returns null instead of showing which players tied
  * no support for more than 2 people with the same hand
  * tie only checks highest tiebreaker card and will return a tie without checking anymore cards
- * player needs .equals method for this to function properly
  */
 /**
  * 
@@ -182,57 +179,62 @@ public class Score {
 	 * @return True if is a Royal Flush
 	 */
 	public boolean isRFlush(ArrayList<Card> hand, ArrayList<Card> commCards) {
-		//TODO this needs to be changed such that the hole cards dont need to be included
-		//Checks that the hole cards are the same suit
-		if(hand.get(0).getSuit() != hand.get(1).getSuit())
-			return false;
-		//Ensures the cards in the hand are A,K,Q,J, or 10
-		if(!(hand.get(0).getValue() >= 10 || hand.get(0).getValue() == 1))
-			return false;
-		if(!(hand.get(1).getValue() >= 10 || hand.get(1).getValue() == 1))
-			return false;
-		
-		//All the requirements for a royal flush
-		char suit = hand.get(0).getSuit();
-		boolean hasAce = false;
-		boolean hasKing = false;
-		boolean hasQueen = false;
-		boolean hasJack = false;
-		boolean has10 = false;
-		
-		//Checks the cards in the hand for royal flush components
-		for(Card c: hand) {
-			if(c.getValue() == 1)//Checking for an ace
-				hasAce = true;
-			else if(c.getValue() == 10)//Checking for a 10
-				has10 = true;
-			else if(c.getValue() == 11)//Checking for a Jack
-				hasJack = true;
-			else if(c.getValue() == 12)//Checking for a Queen
-				hasQueen = true;
-			else if(c.getValue() == 13)//Checking for a King
-				hasKing = true;
+		for(int i = 0; i<4; i++) {
+			//All the requirements for a royal flush
+			char suit;
+			boolean hasAce = false;
+			boolean hasKing = false;
+			boolean hasQueen = false;
+			boolean hasJack = false;
+			boolean has10 = false;
+			
+			if(i==0) {
+				suit = 'H';
+			}
+			else if(i==1) {
+				suit = 'S';
+			}
+			else if(i==2) {
+				suit = 'C';
+			}
+			else{
+				suit = 'D';
+			}
+			
+			//Checks the cards in the hand for royal flush components
+			for(Card c: hand) {
+				if(c.getValue() == 14 && c.getSuit() == suit)//Checking for an ace
+					hasAce = true;
+				else if(c.getValue() == 10 && c.getSuit() == suit)//Checking for a 10
+					has10 = true;
+				else if(c.getValue() == 11 && c.getSuit() == suit)//Checking for a Jack
+					hasJack = true;
+				else if(c.getValue() == 12 && c.getSuit() == suit)//Checking for a Queen
+					hasQueen = true;
+				else if(c.getValue() == 13 && c.getSuit() == suit)//Checking for a King
+					hasKing = true;
+			}
+			
+			//Checks the cards in the Community Cards for royal flush components
+			for(Card c: commCards) {
+				if(c.getValue() == 14 && c.getSuit() == suit)//Checking for an ace
+					hasAce = true;
+				else if(c.getValue() == 10 && c.getSuit() == suit)//Checking for a 10
+					has10 = true;
+				else if(c.getValue() == 11 && c.getSuit() == suit)//Checking for a Jack
+					hasJack = true;
+				else if(c.getValue() == 12 && c.getSuit() == suit)//Checking for a Queen
+					hasQueen = true;
+				else if(c.getValue() == 13 && c.getSuit() == suit)//Checking for a King
+					hasKing = true;
+			}
+			
+			//Checks that all conditions are met
+			if(hasAce && has10 && hasJack && hasQueen && hasKing)
+				return true;
 		}
+		return false;
 		
-		//Checks the cards in the Community Cards for royal flush components
-		for(Card c: commCards) {
-			if(c.getValue() == 1 && c.getSuit() == suit)//Checking for an ace
-				hasAce = true;
-			else if(c.getValue() == 10 && c.getSuit() == suit)//Checking for a 10
-				has10 = true;
-			else if(c.getValue() == 11 && c.getSuit() == suit)//Checking for a Jack
-				hasJack = true;
-			else if(c.getValue() == 12 && c.getSuit() == suit)//Checking for a Queen
-				hasQueen = true;
-			else if(c.getValue() == 13 && c.getSuit() == suit)//Checking for a King
-				hasKing = true;
-		}
-		
-		//Checks that all conditions are met
-		if(hasAce && has10 && hasJack && hasQueen && hasKing)
-			return true;
-		else
-			return false;	
 	}
 	
 	/**
@@ -240,31 +242,45 @@ public class Score {
 	 * @return value of the highest card if is a Straight Flush or 0 if it is not
 	 */
 	public int isSFlush(ArrayList<Card> hand, ArrayList<Card> commCards) {
-		boolean isValid = false;//TODO this needs to be changed such that the hole cards dont need to be included
+		boolean isValid = false;
 		//int max= 14;
-		//Checks that the hole cards are the same suit
-		if(hand.get(0).getSuit() != hand.get(1).getSuit())
-			return 0;
 		
-		char suit = hand.get(0).getSuit();//suit for the straight flush
+		for(int i = 0; i<4; i++) {
+			char suit;
+			
+			if(i==0) {//iterates through the suits
+				suit = 'H';
+			}
+			else if(i==1) {
+				suit = 'S';
+			}
+			else if(i==2) {
+				suit = 'C';
+			}
+			else{
+				suit = 'D';
+			}
+			
+			int highCard = 0; //stores the value of the high card
+			//sets the value of the high card
+			for(Card c: hand) {
+				if(c.getValue() > highCard)//For normal straight will have to check for ace
+					highCard = c.getValue();
+			}
+			for(Card c: commCards) {
+				if(c.getValue() > highCard && c.getSuit() == suit)//For normal straight will have to check for ace
+					highCard = c.getValue();
+			}
 		
-		int highCard = 0; //stores the value of the high card
-		//sets the value of the high card
-		for(Card c: hand) {
-			if(c.getValue() > highCard)//For normal straight will have to check for ace
-				highCard = c.getValue();
-		}
-		for(Card c: commCards) {
-			if(c.getValue() > highCard && c.getSuit() == suit)//For normal straight will have to check for ace
-				highCard = c.getValue();
-		}
+			while(!isValid && highCard > 5) {
+				isValid = isNumHigh(hand, commCards, suit, highCard);
+				if(!isValid)
+					highCard--;
+			}
+			if(isValid)
+				return highCard;
+			}
 		
-		while(!isValid && highCard > 5) {
-			isValid = isNumHigh(hand, commCards, suit, highCard);
-		}
-		if(isValid)
-			return highCard;
-		else 
 			return 0;
 	}
 	
@@ -342,20 +358,92 @@ public class Score {
 	}
 	
 	/**
+	 * Takes in @param highCard and checks if it is the high card in a straight
+	 * @param hand 
+	 * @param commCards
+	 * @param highCard
+	 * @return true if it is a valid highcard
+	 *///used in SFlush
+	public boolean isNumHigh(ArrayList<Card> hand, ArrayList<Card> commCards,  int highCard) {
+		if(highCard < 6)
+			return false;
+		
+		boolean has1Low = false; //has card that is 1 lower than highCard
+		boolean has2Low = false;
+		boolean has3Low = false;
+		boolean has4Low = false;
+		
+		
+		
+		//Check for 1 low
+		for(Card c: hand) {
+			if(c.getValue() == highCard-1)
+				has1Low = true;
+		}
+		for(Card c: commCards) {
+			if(c.getValue() == highCard-1)
+				has1Low = true;
+		}
+		if(!has1Low)
+			return false;
+		
+		//Check for 2 low
+		for(Card c: hand) {
+			if(c.getValue() == highCard-2)
+				has2Low = true;
+		}
+		for(Card c: commCards) {
+			if(c.getValue() == highCard-2)
+				has2Low = true;
+		}
+		if(!has2Low)
+			return false;
+		
+		//Checks for 3 low
+		for(Card c: hand) {
+			if(c.getValue() == highCard-3)
+				has3Low = true;
+		}
+		for(Card c: commCards) {
+			if(c.getValue() == highCard-3)
+				has3Low = true;
+		}
+		if(!has3Low)
+			return false;
+		
+		//Checks for 4 low
+		for(Card c: hand) {
+			if(c.getValue() == highCard-4)
+				has3Low = true;
+		}
+		for(Card c: commCards) {
+			if(c.getValue() == highCard-4)
+				has3Low = true;
+		}
+		if(!has4Low)
+			return false;
+		else
+			return true;
+		
+		
+		
+	}
+	
+	/**
 	 * Finds the highest card of specified suit in either hand or commcards
 	 * @param hand
 	 * @param commCards
 	 * @param suit
 	 * @return
-	 *///Used in sFlush maybe??
+	 *///Used in flush
 	public int findHighest(ArrayList<Card> hand, ArrayList<Card> commCards, char suit) {//TODO figure out if this is used
 		int highCard = 0;
 		for(Card c: hand) {
-			if(c.getValue() > highCard)//For normal straight will have to check for ace
+			if(c.getValue() > highCard)
 				highCard = c.getValue();
 		}
 		for(Card c: commCards) {
-			if(c.getValue() > highCard && c.getSuit() == suit)//For normal straight will have to check for ace
+			if(c.getValue() > highCard && c.getSuit() == suit)
 				highCard = c.getValue();
 		}
 		return highCard;
@@ -366,7 +454,6 @@ public class Score {
 	 * @return value of the card if is Four of a Kind or 0 if its not
 	 */
 	public int isFour(ArrayList<Card> hand, ArrayList<Card> commCards) {
-		//TODO finish
 		//for each card find if there are three others of the same 
 		//make sure that at least one of the four is from the hand
 		//Create an arraylist of all the possible cards for easier checking
@@ -399,27 +486,163 @@ public class Score {
 	 * @return 0 if not a full house: otherwise return value of the three of a kind
 	 */
 	public int isFHouse(ArrayList<Card> hand, ArrayList<Card> commCards) {
-		return 0;//TODO finish full house
+		
+		ArrayList<Card> all = new ArrayList<Card>();
+		for(Card c: hand) {
+			all.add(c);
+		}
+		for(Card c: commCards) {
+			all.add(c);
+		}
+		
+		//requirements for full house
+		boolean hasPair = false;
+		//Brute force iterates through the list finding all value matches
+		//searches for a pair
+		for(Card c: all) {
+			for(Card d: all) {
+				if(c.getValue() == d.getValue() && c.getSuit() != d.getSuit()) {
+					hasPair = true;
+								}}}
+		if(!hasPair) {
+			return 0;
+		}
+		
+		//searches for a three of a kind
+		for(Card c: all) {
+			for(Card d: all) {
+				if(c.getValue() == d.getValue() && c.getSuit() != d.getSuit()) {
+					for(Card e: all) {
+						if(e.getValue() == d.getValue() && e.getSuit() != d.getSuit() && c.getSuit() != e.getSuit()) {
+							return e.getValue();//three of a kind determines tiebreak so it gets passed
+								}}}}}
+		return 0;
 	}
 	
+	/**
+	 * 
+	 * @return 0 if not a flush: otherwise return value of the high card
+	 */
 	public int isFlush(ArrayList<Card> hand, ArrayList<Card> commCards) {
-		return 0;//TODO finish flush
+		
+		ArrayList<Card> all = new ArrayList<Card>();
+		for(Card c: hand) {
+			all.add(c);
+		}
+		for(Card c: commCards) {
+			all.add(c);
+		}
+		//Brute force iterates through the list finding all suit matches
+		for(Card c: all) {
+			for(Card d: all) {
+				if(c.getSuit() == d.getSuit() && !c.equals(d)) {
+					for(Card e: all) {
+						if(e.getSuit() == d.getSuit() && !c.equals(e) && !d.equals(e)) {
+							for(Card f: all) {
+								if(f.getSuit() == d.getSuit() && !c.equals(f) && !d.equals(f) && !e.equals(f)) {
+									for(Card g: all) {
+										if(f.getSuit() == d.getSuit() && !c.equals(g) && !d.equals(g) && !e.equals(g) && !f.equals(g)) {
+											return findHighest(hand, commCards, f.getSuit());
+										}
+									}
+								}}}}}}}
+		return 0;
 	}
 	
+	/**
+	 * 
+	 * @return 0 if not a straight: otherwise return value of the high card
+	 */
 	public int isStraight(ArrayList<Card> hand, ArrayList<Card> commCards) {
-		return 0;//TODO finish straight
+		boolean isValid = false;
+		int highCard = 0; //stores the value of the high card
+		//sets the value of the high card
+		for(Card c: hand) {
+			if(c.getValue() > highCard)
+				highCard = c.getValue();
+		}
+		for(Card c: commCards) {
+			if(c.getValue() > highCard)
+				highCard = c.getValue();
+		}
+	
+		while(!isValid && highCard > 5) {
+			isValid = isNumHigh(hand, commCards, highCard);
+		}
+		if(isValid)
+			return highCard;
+		return 0;
 	}
 	
 	public int isThree(ArrayList<Card> hand, ArrayList<Card> commCards) {
-		return 0;//TODO finish three of a kind
+		//Create an arraylist of all the possible cards for easier checking
+		ArrayList<Card> all = new ArrayList<Card>();
+		for(Card c: hand) {
+			all.add(c);
+		}
+		for(Card c: commCards) {
+			all.add(c);
+		}
+			
+		//Brute force iterates through the list finding all value matches
+		for(Card c: all) {
+			for(Card d: all) {
+				if(c.getValue() == d.getValue() && c.getSuit() != d.getSuit()) {
+					for(Card e: all) {
+						if(e.getValue() == d.getValue() && e.getSuit() != d.getSuit() && c.getSuit() != e.getSuit()) {
+							return e.getValue();
+								}}}}}
+		return 0;
 	}
 	
 	public int is2Pair(ArrayList<Card> hand, ArrayList<Card> commCards) {
-		return 0;//TODO finish two pair
+		//Create an arraylist of all the possible cards for easier checking
+		ArrayList<Card> all = new ArrayList<Card>();
+		for(Card c: hand) {
+			all.add(c);
+		}
+		for(Card c: commCards) {
+			all.add(c);
+		}
+		int pair1 = 0;//value of the first pair
+		int pair2 = 0;
+		//Brute force iterates through the list finding all value matches
+		for(Card c: all) {
+			for(Card d: all) {
+				if(c.getValue() == d.getValue() && c.getSuit() != d.getSuit()) {
+					pair1 = c.getValue();
+								}}}
+		if(pair1 == 0)
+			return 0;
+		
+		for(Card c: all) {
+			for(Card d: all) {
+				if(c.getValue() == d.getValue() && c.getSuit() != d.getSuit()) {
+					pair2 = c.getValue();
+								}}}
+		
+		if(pair2 == 0)
+			return 0;
+		if(pair1 > pair2)
+			return pair1;
+		return pair2;
 	}
 	
 	public int isPair(ArrayList<Card> hand, ArrayList<Card> commCards) {
-		return 0;//TODO finish pair
+		ArrayList<Card> all = new ArrayList<Card>();
+		for(Card c: hand) {
+			all.add(c);
+		}
+		for(Card c: commCards) {
+			all.add(c);
+		}
+		//Brute force iterates through the list finding all value matches
+		for(Card c: all) {
+			for(Card d: all) {
+				if(c.getValue() == d.getValue() && c.getSuit() != d.getSuit()) {
+					return c.getValue();
+								}}}
+		return 0;
 	}
 	
 	public boolean isSameSuit(Card one, Card two) {
